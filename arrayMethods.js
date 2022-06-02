85b3277d713fd14aaa60c0aa621e52ed0a663007
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-02 17:01:12 -0600

    Done but array #6 is not

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+const evenNumbers  = mixedNumbers.filter(function(element){
+  return element % 2 === 0
+})
+//console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -39,10 +41,12 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices  = prices.map(function
+  (element) {
+    return element * 1.07
+  })
 
-
-
+//
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -57,9 +61,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation   = populations.reduce(function(total, element){
+  return total + element
+} )
 
-
+//console.log(totalPopulation)
 
 ////////// PROBLEM 4 //////////
 
@@ -82,7 +88,10 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(function(element) {
+  return element.CP > 200;
+})
+//console.log(myStrongest);
 
 
 
@@ -100,8 +109,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
-
-
+const orderTotals = orders.map(function(element){
+  return element.price + (element.price * element.tax)
+})
+console.log(orderTotals)
 
 ////////// PROBLEM 6 //////////
 
@@ -120,3 +131,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases
+.filter(purchase => purchase.owner === "Bob")
+.reduce((a, b) => a + purchases.price, 0)
+
+console.log(bobsTotal)
